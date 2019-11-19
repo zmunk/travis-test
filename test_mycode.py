6f3c76ca028ce5170619daf3b9593433537b9b6b
Ibrahim Tigrek <ibrahim.tigrek@gmail.com>
2019-11-19 13:51:26 +0300

    Update test_mycode.py

--- test_mycode.py
+++ test_mycode.py
@@ -3,6 +3,6 @@ import mycode
 
 class TestMyCode(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(mycode.add(5, -8), 3)
+        self.assertEqual(mycode.add(5, -8), -3)
         self.assertEqual(mycode.add(5, 3), 8)
         self.assertEqual(mycode.add(-5, -3), -8)
